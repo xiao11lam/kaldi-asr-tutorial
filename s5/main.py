841bbc1d2a281bdff4b4fe83791723f193cec5d8
Xiao Zhang <58770171+xiao11lam@users.noreply.github.com>
2022-09-07 21:45:38 +0100

    Update main.py

--- s5/main.py
+++ s5/main.py
@@ -17,9 +17,13 @@ elif len(sys.argv) == 2:
 else:
     raise ValueError("Too many arguments provided. Aborting")
 
+# Here we can just get the file name. If not just return the first one. 
+print("This is the file for Transcripting:" + FILE_NAME_WAV)
+
 FILE_NAME = FILE_NAME_WAV[:-4]
 
 ORIGINAL_DIRECTORY = os.getcwd()
+print("Set the ORIGINAL_DIRECTORY" + ORIGINAL_DIRECTORY)
 
 # Make data/test dir
 os.makedirs("./data/test")
@@ -30,22 +34,28 @@ os.chdir("./data/test")
 # Assuming one speaker,
 with open("spk2utt", "w") as f:
     f.write("global {0}".format(FILE_NAME))
+print("==============Generated the spk2utt in ./data/test==============")
 
 with open("utt2spk", "w") as f:
     f.write("{0} global".format(FILE_NAME))
+print("==============Generated the utt2spk in ./data/test==============")
+
 
 wav_path = os.getcwd() + "/" + FILE_NAME_WAV
 with open("wav.scp", "w") as f:
     f.write("{0} {1}".format(FILE_NAME, wav_path))
+print("==============Generated the wavscp  in ./data/test==============")
 
 # Return to s5
 os.chdir(ORIGINAL_DIRECTORY)
+print("==============Return to the ORIGINAL_DIRECTORY" + ORIGINAL_DIRECTORY +"==============")
 
 # Identify sample rate in the .wav file
 bash_out = s.run("soxi {0}".format(FILE_NAME_WAV), stdout=s.PIPE, text=True, shell=True)
 cleaned_list = bash_out.stdout.replace(" ","").split('\n')
 sample_rate = [x for x in cleaned_list if x.startswith('SampleRate:')]
 sample_rate = sample_rate[0].split(":")[1]
+print("==============The sample rate in the .wav file is: " + str(sample_rate) + "==============")
 
 # Read mfcc congifuration file
 with open("./conf/mfcc_hires.conf", "r") as mfcc:
@@ -70,42 +80,55 @@ with open("./conf/mfcc_hires.conf", "w") as mfcc:
 
 with open("main_log.txt", "w") as f:
     # Copy wav file into data folder
+    print("==============Copy wav file into data folder==============")
     bash_out = s.run("cp {0} data/test/{0}".format(FILE_NAME_WAV), stdout=f, text=True, shell=True)
-    
     # EXTRACT FEATURES
     # Copy data into new directory
+    print("==============Copy data into new directory==============")
     bash_out = s.run("utils/copy_data_dir.sh data/test data/test_hires", stdout=f, text=True, shell=True)
+
     # Make MFCC features using configuration file provided
+    print("==============Make MFCC features using configuration file provided==============")
     bash_out = s.run("steps/make_mfcc.sh --nj 1 --mfcc-config "
                      "conf/mfcc_hires.conf data/test_hires", stdout=f, text=True, shell=True)
+
     # Compute CMVN statistics
+    print("==============Compute CMVN statistics==============")
     bash_out = s.run("steps/compute_cmvn_stats.sh data/test_hires", stdout=f, text=True, shell=True)
+    
     # Clean the directory
+    print("==============Clean the directory==============")
     bash_out = s.run("utils/fix_data_dir.sh data/test_hires", stdout=f, text=True, shell=True)
 
     # Download pre trained models if don't already have
+    print("==============Download pre trained models if don't already have.==============")
     for component in ["chain", "extractor", "lm"]:
         tarball = "0013_librispeech_v1_{0}.tar.gz".format(component)
         if tarball not in os.listdir():
             bash_out = s.run('wget http://kaldi-asr.org/models/13/{0}'.format(tarball), stdout=f, text=True, shell=True)
     
     # EXTRACT PRE TRAINED MODELS
+    print("==============EXTRACT PRE TRAINED MODELS.==============")
     bash_out = s.run('for f in *.tar.gz; do tar -xvzf "$f"; done', stdout=f, text=True, shell=True)
 
     # DECODE
     # Extract ivectors
+    print("==============DECODE==============")
     os.makedirs("./exp/nnet3_cleaned/ivectors_test_hires")
     bash_out = s.run("steps/online/nnet2/extract_ivectors_online.sh --nj 1 "
                      "data/test_hires exp/nnet3_cleaned/extractor exp/nnet3_cleaned/ivectors_test_hires",
                      stdout=f, text=True, shell=True)
 
+
     # Create decoding graph using the small trigram language model
+    print("==============Create decoding graph using the small trigram language model.==============")    
     os.makedirs("./exp/chain_cleaned/tdnn_1d_sp/graph_tgsmall")
     bash_out = s.run("utils/mkgraph.sh --self-loop-scale 1.0 --remove-oov "
                      "data/lang_test_tgsmall exp/chain_cleaned/tdnn_1d_sp exp/chain_cleaned/tdnn_1d_sp/graph_tgsmall",
                      stdout=f, text=True, shell=True)
 
     # Decode using the created graph
+    print("==============Decode using the created graph==============")    
     os.makedirs("./exp/chain_cleaned/tdnn_1d_sp/decode_test_tgsmall")
     bash_out = s.run("steps/nnet3/decode.sh --acwt 1.0 --post-decode-acwt 10.0 --nj 1 "
                      "--online-ivector-dir exp/nnet3_cleaned/ivectors_test_hires "
@@ -114,6 +137,7 @@ with open("main_log.txt", "w") as f:
                      stdout=f, text=True, shell=True)
 
     # GET TRANSCRIPTION
+    print("==============GET TRANSCRIPTION==============")    
     gz_location = "exp/chain_cleaned/tdnn_1d_sp/decode_test_tgsmall/lat.1.gz"
     words_txt_loc = "{0}/exp/chain_cleaned/tdnn_1d_sp/graph_tgsmall/words.txt".format(ORIGINAL_DIRECTORY)
     command = "../../../src/latbin/lattice-best-path " \
@@ -123,6 +147,7 @@ with open("main_log.txt", "w") as f:
     bash_out = s.run(command, stdout=f, text=True, shell=True)
 
     # RESCORE
+    print("==============RESCORE==============")    
     command = "../../../scripts/rnnlm/lmrescore_pruned.sh --weight 0.45 --max-ngram-order 4 " \
               "data/lang_test_tgsmall exp/rnnlm_lstm_1a data/test_hires " \
               "exp/chain_cleaned/tdnn_1d_sp/decode_test_tgsmall exp/chain_cleaned/tdnn_1d_sp/decode_test_rescore"
